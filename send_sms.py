ca95aace09a30845c3f9e52c06390e814ade88af
Yoanna Wei <yoanna.yw@gmail.com>
2017-10-25 17:37:28 -0400

    changed phone number

--- send_sms.py
+++ send_sms.py
@@ -10,10 +10,9 @@ auth_token  = "fc8b3207974c483f52ed27f707557937"
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+12487192862",
+    to="+15555555555",
     from_="+12018066758",
     body="Hello my puppy! This is kitty's husky app. Reminder to pick up puppy meal.",
     media_url=['http://cdn1-www.dogtime.com/assets/uploads/2016/09/husky-5.jpg'])
 
 print(message.sid)
-
